1f1d0ab68b96446802dad0cf7f3750303e2f8782
willymariino <wg.mariino@gmail.com>
2025-07-30 11:02:23 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+/*
+🎯 Obiettivo:
+
+Aggiungi al carrello
+Incrementa quantità
+Rimuovi dal carrello
+Calcola totale
+
+*/
+
+function App() {
+
+  const gadgets = [
+    { name: 'Smartphone', price: 599 },
+    { name: 'Laptop', price: 999 },
+    { name: 'Tablet', price: 399 },
+    { name: 'Headphones', price: 149 },
+  ];
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
